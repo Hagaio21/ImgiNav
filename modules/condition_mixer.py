8c607186cd28951cee18666b7c5c81c973ce32f5
Hagai <hagayofefr21@gmail.com>
2025-10-02 12:49:16 +0300

    condition mixer skeleton added

---
+++ modules/condition_mixer.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+class ConditionMixer(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self,cond):
+        raise NotImplemented
+
+class ConcatMixer(ConditionMixer):
+    pass
+
+class WeightedMixer(ConditionMixer:
