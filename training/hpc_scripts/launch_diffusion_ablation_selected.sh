dd8f12696c4a11bb0f84af200fe4e4c4d1a35ca5
Hagai Ofer <s233249@student.dtu.dk>
2025-11-04 14:47:53 +0100

    config fix

--- training/hpc_scripts/launch_diffusion_ablation_selected.sh
+++ training/hpc_scripts/launch_diffusion_ablation_selected.sh
@@ -27,8 +27,8 @@ echo ""
 echo "Submitting to L40s queue..."
 # L40s experiments (2 total)
 bsub < "${SCRIPT_DIR}/run_diffusion_capacity_unet64_d5.sh"
-sleep 2
-bsub < "${SCRIPT_DIR}/run_diffusion_scheduler_linear.sh"
+# sleep 2
+# bsub < "${SCRIPT_DIR}/run_diffusion_scheduler_linear.sh"
 
 echo ""
 echo "=========================================="
